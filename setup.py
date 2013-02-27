ab5dc0fac2f9164e31a550c92c858a4aac8f4c8c
Rob V <thisisrobv@gmail.com>
2013-02-26 17:14:39 -0800

    bumping version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'python-firebase',
     packages = find_packages(),
-    version = '0.1.0',
+    version = '0.1.1',
     description = "Simple wrapper around Firebase's REST API",
     author = 'Michael Huynh',
     author_email = 'mike@mikexstudios.com',
